ad00d7ec334bc32aadaf5d47a54a55aa06fe8423
Uday Kumar <udaykumar01200407@gmail.com>
2026-08-04 15:44:12 +0530

    Time: 28 ms (19.18%), Space: 59.2 MB (28.9%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if(s.length!==t.length){
+        return false
+    }
+    let a = s.split("").sort().join("")
+    let b = t.split("").sort().join("")
+    return a === b
+};
