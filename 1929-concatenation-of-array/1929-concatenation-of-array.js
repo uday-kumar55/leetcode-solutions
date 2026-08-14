ec01bc9357c52e42f32a9986aebf3f51a044f74c
Uday Kumar <udaykumar01200407@gmail.com>
2026-08-14 15:46:08 +0530

    Time: 3 ms (15.69%), Space: 57.5 MB (83%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.js
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -3,11 +3,5 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
-    let n = nums.length
-    let arr = new Array((n*2));
-    for(let i =0;i<nums.length;i++){
-        arr[i] = nums[i]
-        arr[n+i] = nums[i]
-    }
-    return arr
+    return [...nums,...nums]
 };
