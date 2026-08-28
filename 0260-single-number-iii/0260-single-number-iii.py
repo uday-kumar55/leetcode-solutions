de784e9855ab1d26f31f57f8f697c39afeee8f85
Uday Kumar <udaykumar01200407@gmail.com>
2026-08-28 13:19:09 +0530

    Time: 1 ms (73.89%), Space: 14.7 MB (12.29%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        f = {}
+        output = []
+        for i in range(0,len(nums)):
+            ch = nums[i]
+            f[ch] = f.get(ch,0)+1
+        for key in f:
+            if f[key] ==1:
+                output.append(int(key))
+        return output
